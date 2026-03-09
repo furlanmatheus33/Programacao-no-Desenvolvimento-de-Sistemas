c63811e77c8f7ff3501a1edc084c64ce9549adf0
furlanmatheus33 <rodrigues.viana.matheus@escola.pr.gov.br>
2026-03-09 11:16:13 -0300

    continuação

---
+++ Aula 1 instricao atribuicao py.py
@@ -0,0 +1,8 @@
+#variavel e um espaço de memoria que tem um nome e guarda algum dado do meu programa
+numero=1
+
+# Expressoes são qualquer combinações de valor, variável e operadores
+resultado=numero + numero + 5
+resultado = (numero * (numero*numero) +33)
+
+print(resultado)
